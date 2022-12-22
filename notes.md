bbbdeaeea3ec9793d2caabc959488379daa2c7e1
Walker Le <walker.le88@gmail.com>
2022-12-21 17:25:03 -0700

    12/21/22 5:24pm

---
+++ notes.md
@@ -0,0 +1,34 @@
+# Core Deliverables
+
+[x] Run json-server --watch db.json
+
+Challenge #1
+[x] Create image for each movie (.createElement, .forEach)
+[x] Append image to #movie-list <nav> (find target, .append())
+
+Challenge #2
+[x] Load first page dataset (capture .movie id=1 Friday the 13th)
+Note have response in fetch to capture first page, but have function to call each detail
+
+Challenge #3
+[x] 'Click' event captures image, title, year, description, watched, blood_amount (event Listener, update html elements)
+[x] Make button settings; 'UnWatched' for false vs 'Watched' for true
+
+Challenge #4
+[] Make button settings toggle 'Watched' and 'Unwatched' when clicked depending on value of watched (from front end? or fetch/backend?) (event listener, PATCH method?)
+
+Challenge #5
+[] Enable more drops for button form
+
+# Data Shape
+{
+    "movies" : [
+        {
+            "id": 1,
+            "title": "Friday the 13th",
+            "release_year": 1980,
+            "description": "Camp counselors are stalked and murdered by an unknown assailant while trying to reopen a summer camp that was the site of a child's drowning.",
+            "image": "./assets/f13-1.jpeg",
+            "watched": false,
+            "blood_amount": 0
+        },
---
+++ src/index.js
@@ -0,0 +1,82 @@
+// Global
+const URL = 'http://localhost:3000/movies'
+
+// DOM Selectors
+const movieList = document.querySelector('#movie-list');
+const movieInfo = document.querySelector('#movie-info'); // not used
+const img = document.querySelector('#detail-image');
+const title = document.querySelector('#title');
+const year = document.querySelector('#year-released');
+const desc = document.querySelector('#description');
+const watched = document.querySelector('#watched');
+const bloodAmount = document.querySelector('#blood-amount');
+const bloodForm = document.querySelector('#blood-form');
+
+// Fetch Functions
+function getMovieMenu(url) {
+    fetch('http://localhost:3000/movies')
+    .then(res => res.json())
+    .then(res => {
+        // console.log(res)
+        renderMovieMenu(res)
+        renderMovie(res[0])
+    })
+}
+
+// Event Listeners
+watched.addEventListener('click', btnToggle);
+
+bloodForm.addEventListener('submit', addBlood);
+
+// Event Handlers
+function btnToggle() {
+    console.log('btnToggle func invoked');
+    // selectedMovie.watched = !selectedMovie.watched
+    // (console.log(watched);)
+    // (watched ? 
+}
+
+function addBlood(e) {
+    e.preventDefault;
+    console.log('addBlood func invoked');
+    bloodForm.reset();
+}
+
+// Render Functions
+function renderMovieMenu(data) {
+    // console.log(data);
+    data.forEach(element => {
+        const movieImg = document.createElement('img');
+        movieImg.src = element.image;
+        // console.log(movieImg.src);
+        // console.log(element.image);
+        movieList.append(movieImg);
+
+        movieImg.addEventListener('click', e => {
+            renderMovie(element);
+            // console.log('I was clicked');
+        })
+    })
+}
+
+function renderMovie(data) {
+    // console.log(data);
+    // console.log(data);
+    // console.log(typeof data);
+    // // console.log(data[0]);
+    // console.log(data.image);
+    img.src = data.image;
+    title.textContent = data.title;
+    year.textContent = data.release_year;
+    desc.textContent = data.description;
+    // watched.textContent = data.watched
+    if (data.watched) {
+        watched.textContent = 'Watched'
+    } else {
+        watched.textContent = 'Unwatched'
+    };
+    bloodAmount.textContent = data.blood_amount;
+}
+
+// Initializers
+getMovieMenu(URL);
